8eb30b6e9d490e83ac0c3dcb85b5cfb6dcb84e7c
kubkowski <jakub.opara@gmail.com>
2015-01-24 17:22:35 +0100

    Added flash test and user scaffold

---
+++ test/responders/flash_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+class FlashTest < ActionController::TestCase
+    tests UserController
+
+    test "sets notice message on successful creation" do
+        post :create, user: { name: "John Doe"}
+        assert_equal "User was successfully created.", flash[:notice]
+    end
+    
+    test "sets notice message on successful update" do
+        user = User.create!(name: "John Doe")
+        put :update, id: user.id, user: { name: "Another John Doe"}
+        assert_equal "User was successfully updated.", flash[:notice]
+    end
+
+    test "sets notice message on successful destroy" do
+        user = User.create!(name: "John Doe")
+        delete :destroy, id: user.id
+        assert_equal "User was successfully destroyed.", flash[:notice]
+    end
+end
